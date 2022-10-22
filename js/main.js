ceb8a67eb0b6aa161ec610ca869d545f21c5d1a2
Noel <noeltrejom16@gmail.com>
2022-10-21 22:40:58 -0600

    new changes 10/21/2022

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 60);
+	});
+   
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+            document.documentElement.style.scrollBehavior = "smooth";
+			$('.ir-arriba').slideDown(300);
+		} else {
+            $('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
